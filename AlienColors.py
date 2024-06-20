7f6ed2e9926ffd31eddc082f9d2f0ecf61a50f4a
ColderAC <adam.l.cook13@gmail.com>
2024-06-19 23:05:53 -0500

    Comparing itmes in list

--- AlienColors.py
+++ AlienColors.py
@@ -45,6 +45,7 @@ else:
 
 
 
+
 favorite_fruits = ['grapes', 'cherries', 'orange']
 if 'apples' in favorite_fruits:
     print("You like Apples")
@@ -60,3 +61,45 @@ if 'cherries' in favorite_fruits:
     print("You like cherries")
 
 
+
+usernames = ['rick','John','admin','casi','FRED']
+if usernames:
+    for username in usernames:
+        if username.lower() == 'admin':
+            print(f"Hello {username}, would you like to see a status report?")
+        else:
+            print(f"Hello {username}!! Welcome back!")    
+else:
+    print("We need to find some users!!")
+
+
+
+
+current_users = ['rick','John','aLLen','casi','FRED','Jack', 'jill']
+new_users = ['robert', 'AllEN', 'jack', 'jacob']
+existing_users = []
+for user in current_users:
+    existing_users.append(user.lower())
+
+for username in new_users:
+    if username.lower() in existing_users:
+        print(f"{username} is already in use. Choose another username.")
+    else:
+        print(f"{username} is avaliable to use!")    
+
+
+
+
+
+
+stored_numbers = [1,2,3,4,5,6,7,8,9]
+for number in stored_numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
+            
